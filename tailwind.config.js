5f3389d5366825c136d8c74e0d3cdb33168fff94
captainLevi17 <migz.levi@gmail.com>
2025-08-10 00:39:24 +1000

    update todo

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/renderer/**/*.{html,js,jsx,ts,tsx}",
+      "./src/**/*.{html,js,jsx,ts,tsx}"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+    darkMode: 'class', // or 'media' or 'class'
+  }
+  
